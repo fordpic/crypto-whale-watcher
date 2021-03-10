d3b7dee1107be6a5e72807845521f668ab24cbd9
fordpic <fordpickert@gmail.com>
2021-03-10 15:02:46 -0600

    currencies component finished

--- src/Currencies.js
+++ src/Currencies.js
@@ -1,10 +1,40 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Currencies = () => {
-    return (
-        <p>List of clickable currencies will go here!</p>
-    )
-}
+	const [currency, setCurrency] = useState([]);
+
+	const getCurrency = () => {
+		const url =
+			'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=decentralized_finance&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h';
+
+		fetch(url)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+				setCurrency(res);
+			});
+	};
+	useEffect(() => {
+		getCurrency();
+	}, []);
+	console.log(currency);
+	return (
+		<div className='coin-info'>
+			<h1>Top 100 Coins by Market Cap</h1>
+			{currency.map((coin) => {
+				return (
+					<div key={coin.name}>
+						<Link to={`/currencies/${coin.name}`}>
+							<h1>
+								{coin.name} - {coin.symbol}
+							</h1>
+						</Link>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default Currencies;
--- src/Home.js
+++ src/Home.js
@@ -3,41 +3,48 @@ import React, { useState, useEffect } from 'react';
 
 const Home = () => {
 
-const [homeInfo, setHomeInfo] = useState([]);
+    const [homeInfo, setHomeInfo] = useState([]);
 
-const getHomeInfo = () => {
-    const url =
-			'https://api.coingecko.com/api/v3/global/decentralized_finance_defi';
-    
-    fetch(url)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res);
-        setHomeInfo(res);
-    })
-}
-console.log(homeInfo)
+    const getHomeInfo = () => {
+        const url =
+                'https://api.coingecko.com/api/v3/global/decentralized_finance_defi';
+        
+        fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            // console.log(res);
+            setHomeInfo(res);
+        })
+    }
     useEffect(() => {
         getHomeInfo()
     }, [])
 
+    // console.log(homeInfo.data)
 
-
-    return (
-			<div className='home-info'>
+return (
+    <div className='home-info'>
+               { homeInfo ? (
+                   <div>
 				<p>
-					Welcome to Crypto Whale Watcher! <br></br>Below you will see the
-					current state of Defi (Decentralized Finance) as a whole, but please
-					select any option from above to go ahead and get started!
+					Welcome to Crypto Whale Watcher! <br></br>
+					<br></br>Below you will see the current state of Defi (Decentralized
+					Finance) as a whole, but please select any option from above to go
+					ahead and get started!
 				</p>
-				<p>DeFi Dominance: {homeInfo.data.defi_dominance}%</p>
-				<p>DeFi Market Cap: {homeInfo.data.defi_market_cap}%</p>
-				<p>DeFi To Ethereum Ratio: {homeInfo.data.defi_to_eth_ratio}%</p>
-				<p>Ethereum Market Cap: {homeInfo.data.defi_dominance}%</p>
-				<p>Top Coin (Current): {homeInfo.data.top_coin_name}</p>
-				<p>Trading Volume (last 24 hours): ${homeInfo.data.trading_volume_24h}</p>
+				<br></br>
+				<p>DeFi Dominance: {homeInfo?.data?.defi_dominance}%</p>
+				<p>DeFi Market Cap: {homeInfo?.data?.defi_market_cap}%</p>
+				<p>DeFi To Ethereum Ratio: {homeInfo?.data?.defi_to_eth_ratio}%</p>
+				<p>Ethereum Market Cap: {homeInfo?.data?.defi_dominance}%</p>
+				<p>Top Coin (Current): {homeInfo?.data?.top_coin_name}</p>
+				<p>
+					Trading Volume (last 24 hours): ${homeInfo?.data?.trading_volume_24h}
+				</p>
+                </div>
+                ) : null};
+                
 			</div>
-		);
-}
+)}
 
 export default Home;
