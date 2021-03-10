aac49cdda43fda64c69134d6176d328305ac2b87
fordpic <fordpickert@gmail.com>
2021-03-10 17:51:42 -0600

    ready to start styling

--- src/CurrencyInfo.js
+++ src/CurrencyInfo.js
@@ -3,14 +3,12 @@ import React, { useState, useEffect } from 'react';
 const coinGeckoURL = 'https://api.coingecko.com/api/v3/coins/';
 
 const CurrencyInfo = ({ match }) => {
-	console.log(match);
-
 	const [coinInfo, setCoinInfo] = useState([]);
 
 	useEffect(() => {
 		const coinName = match.params.id.toLowerCase();
 		const url = `${coinGeckoURL}${coinName}`;
-		console.log(coinName);
+		// console.log(coinName);
 		fetch(url)
 			.then((res) => res.json())
 			.then((res) => {
@@ -28,15 +26,15 @@ const CurrencyInfo = ({ match }) => {
 				<img src={coinInfo.image?.large} alt={coinInfo.name} />
 				<div className='coin-details'>
 					<h1>{coinInfo.name}</h1>
-					<h2>Current Price: {coinInfo.market_data?.current_price.usd}</h2>
+					<h2>Current Price: ${coinInfo.market_data?.current_price.usd}</h2>
 					<h2>
-						Change in Price (last 24 hours):{' '}
-						{coinInfo.market_data?.price_change_24h}
-						{coinInfo.market_data?.price_change_percentage_24h}
+						Change in Price (last 24 hours): $
+						{coinInfo.market_data?.price_change_24h}{' '}
+						{coinInfo.market_data?.price_change_percentage_24h}%
 					</h2>
 					<h2>
 						Change in Price (last 7 days):{' '}
-						{coinInfo.market_data?.price_change_percentage_7d}
+						{coinInfo.market_data?.price_change_percentage_7d}%
 					</h2>
 					<h2>Market Cap: {coinInfo.market_data?.market_cap.usd}</h2>
 					<h2>Market Cap Rank: {coinInfo.market_data?.market_cap_rank}</h2>
@@ -44,13 +42,12 @@ const CurrencyInfo = ({ match }) => {
 						Change in Market Cap (last 24 hours):{' '}
 						{coinInfo.market_data?.market_cap_change_24h}
 					</h2>
-					<h2>All Time High: {coinInfo.market_data?.ath.usd}</h2>
+					<h2>All Time High: ${coinInfo.market_data?.ath.usd}</h2>
 					<h2>All Time High Date: {coinInfo.market_data?.ath_date.usd}</h2>
-					<h2>All Time Low: {coinInfo.market_data?.atl.usd}</h2>
+					<h2>All Time Low: ${coinInfo.market_data?.atl.usd}</h2>
 					<h2>All Time Low Date: {coinInfo.market_data?.atl_date.usd}</h2>
 					<h2>Market Cap Rank: {coinInfo.market_cap_rank}</h2>
-					<h2>Total Supply: {coinInfo.total_supply}</h2>
-					<p>{coinInfo.description?.ar}</p>
+					<p>{coinInfo.description?.en}</p>
 				</div>
 			</div>
 		</div>
--- src/Home.js
+++ src/Home.js
@@ -1,50 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
-
 const Home = () => {
+	const [homeInfo, setHomeInfo] = useState([]);
 
-    const [homeInfo, setHomeInfo] = useState([]);
+	const getHomeInfo = () => {
+		const url =
+			'https://api.coingecko.com/api/v3/global/decentralized_finance_defi';
 
-    const getHomeInfo = () => {
-        const url =
-                'https://api.coingecko.com/api/v3/global/decentralized_finance_defi';
-        
-        fetch(url)
-        .then(res => res.json())
-        .then(res => {
-            // console.log(res);
-            setHomeInfo(res);
-        })
-    }
-    useEffect(() => {
-        getHomeInfo()
-    }, [])
+		fetch(url)
+			.then((res) => res.json())
+			.then((res) => {
+				// console.log(res);
+				setHomeInfo(res);
+			});
+	};
+	useEffect(() => {
+		getHomeInfo();
+	}, []);
 
-    // console.log(homeInfo.data)
+	// console.log(homeInfo.data)
 
-return (
-    <div className='home-info'>
-               { homeInfo ? (
-                   <div>
-				<p>
-					Welcome to Crypto Whale Watcher! <br></br>
-					<br></br>Below you will see the current state of Defi (Decentralized
-					Finance) as a whole, but please select any option from above to go
-					ahead and get started!
-				</p>
-				<br></br>
-				<p>DeFi Dominance: {homeInfo?.data?.defi_dominance}%</p>
-				<p>DeFi Market Cap: {homeInfo?.data?.defi_market_cap}%</p>
-				<p>DeFi To Ethereum Ratio: {homeInfo?.data?.defi_to_eth_ratio}%</p>
-				<p>Ethereum Market Cap: {homeInfo?.data?.defi_dominance}%</p>
-				<p>Top Coin (Current): {homeInfo?.data?.top_coin_name}</p>
-				<p>
-					Trading Volume (last 24 hours): ${homeInfo?.data?.trading_volume_24h}
-				</p>
-                </div>
-                ) : null};
-                
-			</div>
-)}
+	return (
+		<div className='home-info'>
+			{homeInfo ? (
+				<div>
+					<p>
+						Welcome to Crypto Whale Watcher! <br></br>
+						<br></br>Below you will see the current state of Defi (Decentralized
+						Finance) as a whole, but please select any option from above to go
+						ahead and get started!
+					</p>
+					<br></br>
+					<p>DeFi Dominance: {homeInfo?.data?.defi_dominance}%</p>
+					<p>DeFi Market Cap: {homeInfo?.data?.defi_market_cap}%</p>
+					<p>DeFi To Ethereum Ratio: {homeInfo?.data?.defi_to_eth_ratio}%</p>
+					<p>Ethereum Market Cap: {homeInfo?.data?.defi_dominance}%</p>
+					<p>Top Coin (Current): {homeInfo?.data?.top_coin_name}</p>
+					<p>
+						Trading Volume (last 24 hours): $
+						{homeInfo?.data?.trading_volume_24h}
+					</p>
+				</div>
+			) : null}
+			;
+		</div>
+	);
+};
 
 export default Home;
